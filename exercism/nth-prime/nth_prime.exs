062e76b6437ffeb7c03b44514d472c9bfeca3758
defndaines <defndaines@gmail.com>
2017-02-01 04:28:04 -0800

    Adds more Exercism solutions

---
+++ exercism/nth-prime/nth_prime.exs
@@ -0,0 +1,14 @@
+defmodule Prime do
+
+  @doc """
+  Generates the nth prime.
+  """
+  @spec nth(non_neg_integer) :: non_neg_integer
+  def nth(1), do: 2
+  def nth(2), do: 3
+  def nth(count) when count > 2 do
+    Stream.iterate(3, &(&1 + 2))
+    5
+  end
+
+end
