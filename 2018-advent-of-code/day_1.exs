343fb52a27816a7d36e3713de2a0526d4fcf64e8
Michael Daines <953037+defndaines@users.noreply.github.com>
2018-12-02 17:50:27 -0800

    Advent of Code 2018: Day 1, Part Two

--- 2018-advent-of-code/day_1.exs
+++ 2018-advent-of-code/day_1.exs
@@ -1,9 +1,52 @@
 defmodule Day1 do
-  def calibrate([], acc) do
-    acc
+  @moduledoc """
+  Solutions to Day 1 of 2018 Advent of Code.
+  https://adventofcode.com/2018/day/1
+  """
+
+  @doc """
+  Read a list of modulations (positive or negative integers) from a file.
+
+  Returns a list of integer values for each line in the input file.
+  """
+  def input_to_list() do
+    {:ok, input} = File.read("input")
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
   end
+
+  @doc """
+  Iterate through a list of frequency calibrations, accumulating each one.
+
+  Returns the final result of applying all calibrations.
+  """
+  def calibrate([], acc), do: acc
   def calibrate([head | tail], acc) do
     calibrate(tail, acc + head)
+  end
+
+  @doc """
+  Cycle through a list of frequency calibrations, keeping track of each
+  frequency seen so far. The calibrations continue back at the start of the
+  list when the end of the list has been reached but a repeat has not yet been
+  found.
+
+  Returns the first frequency that is arrived at twice.
+  """
+  def first_repeat(base) do
+    first_repeat(base, 0, %{0 => 1}, base)
+  end
+
+  def first_repeat([], acc, seen, base) do
+    first_repeat(base, acc, seen, base)
+  end
+  def first_repeat([head | tail], acc, seen, base) do
+    freq = head + acc
+    case Map.get(seen, freq) do
+      nil -> first_repeat(tail, freq, Map.put(seen, freq, 1), base)
+      _ -> freq
+    end
   end
 end
 
@@ -14,12 +57,13 @@ defmodule Day1Test do
 
   import Day1
 
-  test "input" do
-    {:ok, input} = File.read("input")
-    result = input
-             |> String.split("\n", trim: true)
-             |> Enum.map(&String.to_integer/1)
-             |> calibrate(0)
+  test "part one" do
+    result = calibrate(input_to_list(), 0)
     IO.puts(result)
+  end
+
+  test "part two" do
+    repeater = first_repeat(input_to_list())
+    IO.puts(repeater)
   end
 end
