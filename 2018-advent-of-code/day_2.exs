650f10bda031c3fdc6cacb51e0ea840ec68339b4
Michael Daines <953037+defndaines@users.noreply.github.com>
2018-12-03 20:50:46 -0800

    Advent of Code 2018: Day 2, Part Two

--- 2018-advent-of-code/day_2.exs
+++ 2018-advent-of-code/day_2.exs
@@ -40,6 +40,40 @@ defmodule Day2 do
     twos * threes
   end
 
+  defp distance([], [], acc), do: {:ok, acc}
+  defp distance(_, _, acc) when acc > 1, do: {:too_large, acc}
+  defp distance([c | rest_1], [c | rest_2], acc) do
+    distance(rest_1, rest_2, acc)
+  end
+  defp distance([_ | rest_1], [_ | rest_2], acc) do
+    distance(rest_1, rest_2, acc + 1)
+  end
+
+  def close_ids([id | ids]) do
+    id_list = String.to_charlist(id)
+    candidates = Enum.filter(ids, fn x ->
+      {k, _} = distance(id_list, String.to_charlist(x), 0)
+      k == :ok
+    end)
+
+    case candidates do
+      [] -> close_ids(ids)
+      [match | _] -> commonalities({id, match})
+    end
+  end
+
+  defp matching_char_reducer(e, acc) do
+    case e do
+      {a, a} -> [a | acc]
+      _ -> acc
+    end
+  end
+
+  defp commonalities({id_1, id_2}) do
+    Enum.zip(String.to_charlist(id_1), String.to_charlist(id_2))
+    |> Enum.reduce([], &matching_char_reducer/2)
+    |> Enum.reverse
+  end
 end
 
 ExUnit.start()
@@ -55,6 +89,8 @@ defmodule Day2Test do
   end
 
   test "part two" do
+    result = close_ids(input_to_list())
+    IO.puts(result)
   end
 end
 
