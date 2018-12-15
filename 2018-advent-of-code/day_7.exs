80e0f1d9ba820d7aa694cc8e25af8bcf4bb5cb44
Michael Daines <953037+defndaines@users.noreply.github.com>
2018-12-15 11:30:46 -0800

    Advent of Code 2018: Day 7, Part One

--- 2018-advent-of-code/day_7.exs
+++ 2018-advent-of-code/day_7.exs
@@ -16,27 +16,27 @@ defmodule Day7 do
   """
   def input_to_list() do
     {:ok, input} = File.read("input")
-    String.split(input, "\n", trim: true)
+    input
+    |> String.split("\n", trim: true)
     |> Enum.map(fn line -> Regex.run(@line_regex, line, [capture: :all_but_first]) end)
   end
 
   def map_instructions(input) do
     graph = :digraph.new
-    all_vertices = Enum.reduce(input, &Enum.concat/2) |> Enum.uniq
+    all_vertices = input |> Enum.reduce(&Enum.concat/2) |> Enum.uniq
     for ch <- all_vertices, do: :digraph.add_vertex(graph, ch)
     for [from, to] <- input, do: :digraph.add_edge(graph, from, to)
     graph
   end
 
-  def ready_neighbors(graph, node, visited, free) do
-    opened = :digraph.out_neighbours(graph, node)
-    avail = MapSet.new(free)
-    been_there = MapSet.new(visited)
-    Enum.filter(opened,
-      # in_neigh =  MapSet.new(:digraph.in_neighbours(graph, n))
-      #  available? ... MapSet.subset?(in_neigh, been_there)
-      fn n -> 
-
+  defp step(_graph, acc, _visited, []), do: acc
+  defp step(graph, acc, visited, [node | free]) do
+    visit_set = MapSet.put(visited, node)
+    new_neighbors = graph
+                    |> :digraph.out_neighbours(node)
+                    |> Enum.filter(fn v -> :digraph.in_neighbours(graph, v) |> MapSet.new |> MapSet.subset?(visit_set) end)
+    free_now = free ++ new_neighbors |> Enum.sort |> Enum.dedup
+    step(graph, acc <> node, visit_set, free_now)
   end
 
   @doc """
@@ -45,13 +45,11 @@ defmodule Day7 do
   """
   def order_instructions(input) do
     graph = map_instructions(input)
-    vertices = :digraph.vertices(graph)
-    starting = Enum.filter(vertices, fn v -> :digraph.in_neighbours(graph, v) == [] end) |> Enum.sort
-    [start | rem] = starting
-    visited = [start]
-    next_options = rem ++ :digraph.out_neighbours(graph, start)
-
-    :digraph.out_neighbours(graph, start)
+    starting = graph
+               |> :digraph.vertices
+               |> Enum.filter(fn v -> :digraph.in_neighbours(graph, v) == [] end)
+               |> Enum.sort
+    step(graph, "", MapSet.new, starting)
   end
 end
 
@@ -65,7 +63,7 @@ defmodule Day7Test do
   test "input to list" do
     input = input_to_list()
 
-    [ ["U", "A"],
+    [["U", "A"],
       ["F", "Z"],
       ["B", "J"]] = Enum.take(input, 3)
   end
@@ -85,7 +83,8 @@ defmodule Day7Test do
   end
 
   test "part one" do
-    # IO.puts()
+    result = order_instructions(input_to_list())
+    IO.puts(result)
   end
 
   test "part two" do
